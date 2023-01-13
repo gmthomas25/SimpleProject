ff3fa38cc27158511a4c6464def819063f20670f
godso <godso@DESKTOP-DR2NBDA.mynetworksettings.com>
2023-01-13 18:33:26 -0500

    second commit

--- src/test/java/classes/StringEx.java
+++ src/test/java/classes/StringEx.java
@@ -2,4 +2,31 @@ package classes;
 
 public class StringEx {
 
+	public static void halfSize(String str) {
+		
+	}
+	
+	public static String toUpper(String str) {
+		return "";
+	}
+	
+	public static String toLower(String str) {
+		return "";
+	}
+	
+	public static void equal(String str1, String str2) {
+		
+	}
+	
+	public static void reverse(String str) {
+		
+	}
+	
+	public static char findCharacter(String str, int index) {
+		return ' ';
+	}
+	
+	public static String append(String str, String str2) {
+		return "";
+	}
 }
